532e02f48c18cc774eb4d78ffdcac75fa6fbd725
Virtanen <virtanj@jyu.fi>
2023-06-27 14:11:17 +0300

    alus liikkuu!

--- avaruuspeli/avaruuspeli/avaruuspeli.cs
+++ avaruuspeli/avaruuspeli/avaruuspeli.cs
@@ -9,30 +9,40 @@ namespace avaruuspeli;
 
 public class avaruuspeli : PhysicsGame
 {
+    
     private PhysicsObject _alus;
+    private ListenContext ohjaimet;
     public override void Begin()
     {
         Alus();
         Kentta();
         Ohjaukset();
 
-        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+        PhysicsObject kuutio = new PhysicsObject(50, 50);
+        kuutio.Shape = Shape.Diamond;
+        kuutio.X = 100;
+        Add(kuutio);
     }
 
     private void Alus()
     {
         _alus = new PhysicsObject(50, 50);
-        _alus.Shape = Jypeli.Shape.Circle;
+        _alus.Shape = Jypeli.Shape.Diamond;
         _alus.Color = Color.Red;
         Add(_alus);
+        
+        Camera.Follow(_alus);
     }
 
     private void Ohjaukset()
     {
-        Mouse.Listen(MouseButton.Right, ButtonState.Down, Eteenpain, "Liiku hiiren suuntaan.");
-        Mouse.Listen(MouseButton.Right, ButtonState.Up, Pysaytys, null);
-        Mouse.Listen(MouseButton.Left, ButtonState.Pressed, Ammu, "Ammu hiiren suuntaan.");
+        ohjaimet = ControlContext.CreateSubcontext();
         
+        //Mouse.Listen(MouseButton.Left, ButtonState.Pressed, Eteenpain, "Liiku hiiren suuntaan.");
+        //Mouse.Listen(MouseButton.Left, ButtonState.Released, Pysaytys, null);
+        Mouse.Listen(MouseButton.Right, ButtonState.Down, Ammu, "Ammu hiiren suuntaan.");
+        Mouse.ListenMovement(0.1, KuunteleLiiketta, null); //.InContext(ohjaimet);
+
     }
 
     private void Kentta()
@@ -42,25 +52,25 @@ public class avaruuspeli : PhysicsGame
 
     private void Eteenpain()
     {
-        Mouse.ListenMovement(0.1, KuunteleLiiketta, null);
+        ohjaimet.Enable();
     }
 
     private void Pysaytys()
     {
-        _alus.X = _alus.X;
-        _alus.Y = _alus.Y;
+        ohjaimet.Disable();
     }
 
     private void Ammu()
     {
         
     }
-    void KuunteleLiiketta()
-    {        
-        _alus.X = Mouse.PositionOnWorld.X;
-        _alus.Y = Mouse.PositionOnWorld.Y;
+    private void KuunteleLiiketta()
+    {
+        //Vector paikkaRuudulla = Mouse.PositionOnWorld;
+        //_alus.MoveTo(paikkaRuudulla, 500);
 
-        Vector hiirenLiike = Mouse.MovementOnWorld; // tai Mouse.MovementOnScreen
-        // tähän hiiren liikevektorin hyödyntäminen
+        Vector hiiri = Mouse.PositionOnWorld - _alus.Position;
+        _alus.MoveTo(hiiri * Int32.MaxValue, 500);
+
     }
 }
