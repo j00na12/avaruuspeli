c89ad06212065ac917b241d32c5facfe6581705e
Virtanen <virtanj@jyu.fi>
2023-06-27 14:54:54 +0300

    koitin saada kuvan

--- avaruuspeli/avaruuspeli/avaruuspeli.cs
+++ avaruuspeli/avaruuspeli/avaruuspeli.cs
@@ -9,7 +9,9 @@ namespace avaruuspeli;
 
 public class avaruuspeli : PhysicsGame
 {
-    
+    private Image avaruusTausta = LoadImage("AvaruusTausta.png");
+    private Image alusKuva = LoadImage("Alus.png");
+
     private PhysicsObject _alus;
     private ListenContext ohjaimet;
     public override void Begin()
@@ -18,7 +20,7 @@ public class avaruuspeli : PhysicsGame
         Kentta();
         Ohjaukset();
 
-        PhysicsObject kuutio = new PhysicsObject(50, 50);
+        PhysicsObject kuutio = new PhysicsObject(32, 32);
         kuutio.Shape = Shape.Diamond;
         kuutio.X = 100;
         Add(kuutio);
@@ -26,11 +28,12 @@ public class avaruuspeli : PhysicsGame
 
     private void Alus()
     {
-        _alus = new PhysicsObject(50, 50);
-        _alus.Shape = Jypeli.Shape.Diamond;
-        _alus.Color = Color.Red;
-        Add(_alus);
+        _alus = new PhysicsObject(32, 32);
+        _alus.Shape = Shape.FromImage(alusKuva);
         
+        Add(_alus);
+
+        Camera.ZoomFactor = 2;
         Camera.Follow(_alus);
     }
 
@@ -70,7 +73,7 @@ public class avaruuspeli : PhysicsGame
         //_alus.MoveTo(paikkaRuudulla, 500);
 
         Vector hiiri = Mouse.PositionOnWorld - _alus.Position;
-        _alus.MoveTo(hiiri * Int32.MaxValue, 500);
+        _alus.MoveTo(hiiri * Int32.MaxValue, 300);
 
     }
 }
