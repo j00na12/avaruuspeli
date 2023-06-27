b5b5c527939688d7162da44105b71a8015fbe196
Virtanen <virtanj@jyu.fi>
2023-06-27 11:41:54 +0300

    alus melkein liikkuu

--- avaruuspeli/avaruuspeli/avaruuspeli.cs
+++ avaruuspeli/avaruuspeli/avaruuspeli.cs
@@ -9,11 +9,58 @@ namespace avaruuspeli;
 
 public class avaruuspeli : PhysicsGame
 {
+    private PhysicsObject _alus;
     public override void Begin()
     {
-        // Kirjoita ohjelmakoodisi tähän
+        Alus();
+        Kentta();
+        Ohjaukset();
 
-        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+    }
+
+    private void Alus()
+    {
+        _alus = new PhysicsObject(50, 50);
+        _alus.Shape = Jypeli.Shape.Circle;
+        _alus.Color = Color.Red;
+        Add(_alus);
+    }
+
+    private void Ohjaukset()
+    {
+        Mouse.Listen(MouseButton.Right, ButtonState.Down, Eteenpain, "Liiku hiiren suuntaan.");
+        Mouse.Listen(MouseButton.Right, ButtonState.Up, Pysaytys, null);
+        Mouse.Listen(MouseButton.Left, ButtonState.Pressed, Ammu, "Ammu hiiren suuntaan.");
+        
+    }
+
+    private void Kentta()
+    {
+        Level.Background.Color = Color.Black;
+    }
+
+    private void Eteenpain()
+    {
+        Mouse.ListenMovement(0.1, KuunteleLiiketta, null);
+    }
+
+    private void Pysaytys()
+    {
+        _alus.X = _alus.X;
+        _alus.Y = _alus.Y;
+    }
+
+    private void Ammu()
+    {
+        
+    }
+    void KuunteleLiiketta()
+    {        
+        _alus.X = Mouse.PositionOnWorld.X;
+        _alus.Y = Mouse.PositionOnWorld.Y;
+
+        Vector hiirenLiike = Mouse.MovementOnWorld; // tai Mouse.MovementOnScreen
+        // tähän hiiren liikevektorin hyödyntäminen
     }
 }
