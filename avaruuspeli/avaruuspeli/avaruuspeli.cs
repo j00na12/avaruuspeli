63efa55e2a3c7a61376205c18d1a189eaf1ef6e4
Virtanen <virtanj@jyu.fi>
2023-06-28 11:04:28 +0300

    Pitää saada alus kääntymään

--- avaruuspeli/avaruuspeli/avaruuspeli.cs
+++ avaruuspeli/avaruuspeli/avaruuspeli.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using FarseerPhysics.Dynamics;
 using Jypeli;
 using Jypeli.Assets;
 using Jypeli.Controls;
@@ -9,13 +10,15 @@ namespace avaruuspeli;
 
 public class avaruuspeli : PhysicsGame
 {
-    private Image avaruusTausta = LoadImage("AvaruusTausta.png");
-    private Image alusKuva = LoadImage("Alus.png");
+    private Image _avaruusTausta = LoadImage("AvaruusTausta.png");
+    private Image _aluksenKuva = LoadImage("Alus.png");
 
     private PhysicsObject _alus;
-    private ListenContext ohjaimet;
+    //private ListenContext ohjaimet;
+    
     public override void Begin()
     {
+        //AloitusNayttö();
         Alus();
         Kentta();
         Ohjaukset();
@@ -29,31 +32,40 @@ public class avaruuspeli : PhysicsGame
     private void Alus()
     {
         _alus = new PhysicsObject(32, 32);
-        _alus.Shape = Shape.FromImage(alusKuva);
+        
+        _aluksenKuva.Scaling = ImageScaling.Nearest;
+        _alus.Image = _aluksenKuva;
         
         Add(_alus);
 
         Camera.ZoomFactor = 2;
         Camera.Follow(_alus);
+
+        Angle aluksenKulma = _alus.Angle;
+        
     }
 
     private void Ohjaukset()
     {
-        ohjaimet = ControlContext.CreateSubcontext();
+        //ohjaimet = ControlContext.CreateSubcontext();
         
         //Mouse.Listen(MouseButton.Left, ButtonState.Pressed, Eteenpain, "Liiku hiiren suuntaan.");
         //Mouse.Listen(MouseButton.Left, ButtonState.Released, Pysaytys, null);
         Mouse.Listen(MouseButton.Right, ButtonState.Down, Ammu, "Ammu hiiren suuntaan.");
         Mouse.ListenMovement(0.1, KuunteleLiiketta, null); //.InContext(ohjaimet);
-
+        
+        
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, Paussilla, "Paussittaa pelin");
     }
 
     private void Kentta()
     {
         Level.Background.Color = Color.Black;
+        Level.Background.Image = _avaruusTausta;
+        Level.Background.TileToLevel();
     }
 
-    private void Eteenpain()
+    /* private void Eteenpain()
     {
         ohjaimet.Enable();
     }
@@ -61,12 +73,29 @@ public class avaruuspeli : PhysicsGame
     private void Pysaytys()
     {
         ohjaimet.Disable();
+    } */
+    private void Paussilla()
+    {
+        Pause();
+        MultiSelectWindow paussiMenu = new MultiSelectWindow("Paussilla", "Jatka", "Parhaat pisteet", "Lopeta");
+        paussiMenu.AddItemHandler(0, Jatka);
+        paussiMenu.AddItemHandler(2, Exit);
+        paussiMenu.DefaultCancel = 0;
+        Add(paussiMenu);
+        
+        paussiMenu.Color = Color.DarkGray;
+        paussiMenu.SetButtonColor(Color.MidnightBlue);
     }
 
+    private void Jatka()
+    {
+        Pause();
+    }
     private void Ammu()
     {
         
     }
+    
     private void KuunteleLiiketta()
     {
         //Vector paikkaRuudulla = Mouse.PositionOnWorld;
@@ -76,4 +105,28 @@ public class avaruuspeli : PhysicsGame
         _alus.MoveTo(hiiri * Int32.MaxValue, 300);
 
     }
+
+    /*private void AloitusNayttö()
+    {
+        MultiSelectWindow alkuvalikko = new MultiSelectWindow("Avaruus Peli!", "Aloita peli", "Parhaat pisteet", "Lopeta");
+        Add(alkuvalikko);
+        
+        alkuvalikko.AddItemHandler(0, AloitaPeli);
+        alkuvalikko.AddItemHandler(1, ParhaatPisteet);
+        alkuvalikko.AddItemHandler(2, Exit);
+        alkuvalikko.DefaultCancel = -1;
+        
+        alkuvalikko.Color = Color.DarkGray;
+        alkuvalikko.SetButtonColor(Color.MidnightBlue);
+    }
+
+    private void AloitaPeli()
+    {
+        
+    }
+
+    private void ParhaatPisteet()
+    {
+        
+    }*/
 }
